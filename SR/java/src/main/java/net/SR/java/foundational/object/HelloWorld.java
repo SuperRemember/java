8504d923599032e84c94a8393154a1c5ae45dd08
SuperRemember <171381790@qq.com>
2022-06-10 11:04:47 +0800

    First JAVA

---
+++ SR/java/src/main/java/net/SR/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.SR.java.foundational.object;
+
+/**
+ * First Java
+ *
+ * @author 1713817920@qq.com
+ * @version 2022/6/10 11:02
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
